215052e426179c7e866d9e8807b854a4877ea97f
Olivier Tremblay-Boulet <melonspeedruns@outlook.com>
2021-03-05 15:45:46 -0500

    made the default console state false

--- Ryujinx/Configuration/ConfigurationState.cs
+++ Ryujinx/Configuration/ConfigurationState.cs
@@ -518,6 +518,7 @@ namespace Ryujinx.Configuration
             System.EnableDockedMode.Value          = true;
             EnableDiscordIntegration.Value         = true;
             CheckUpdatesOnStart.Value              = true;
+            ShowConsole.Value                      = false;
             ShowConfirmExit.Value                  = true;
             HideCursorOnIdle.Value                 = false;
             ShowConsole.Value                      = true;
